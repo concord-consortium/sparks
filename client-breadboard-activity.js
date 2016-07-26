26bd911c6f9e47e76796ef2e2d8c986b5d2cdc03
Doug Martin <doug@zoopdoop.com>
2016-07-26 04:33:10 -0400

    Fix SVG bug that prevents moving to the second activity in this sequence for SPARKS voltage divider activity [#126636725]
    The breadboard svg html was being cleared and hidden.  The clearing caused the "go to next level" button to error because the svg did not exist.

--- client-breadboard-activity.js
+++ client-breadboard-activity.js
@@ -3364,7 +3364,7 @@ sparks.createQuestionsCSV = function(data) {
       $('.report').html('');
       if (!!finalReport){
         $('#image').html('');
-        $('#breadboard_wrapper').children().html('').hide();
+        $('#breadboard_wrapper').children().hide();
       }
       this.$reportDiv = $('<div>').addClass('report').css('float', 'left').css('padding-top', '15px').css('padding-left', '40px');
       this.$reportDiv.append($report);
